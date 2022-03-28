53df072b379832f9614f0beffc124218b4e8e68f
henriqueholtz <henrique_holtz@hotmail.com>
2022-03-28 06:23:36 -0300

    Use Footer in MyApp component + CssBaseLine from @material-ui/core

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
+import { CssBaseline } from '@material-ui/core';
 import type { AppProps } from 'next/app';
+import { Footer } from '../components';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
