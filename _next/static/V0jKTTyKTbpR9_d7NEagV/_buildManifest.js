9783111aa4846761dfbab520218f99ecc5dc55fe
henriqueholtz <henrique_holtz@hotmail.com>
2022-03-28 07:37:22 +0000

    Deploying to gh-pages from @ henriqueholtz/henriqueholtz.github.io@5210f8d38b8ca191684206bc7ef9c364002f5c97 🚀

--- _next/static/V0jKTTyKTbpR9_d7NEagV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3c3692e81f78c9c0.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
