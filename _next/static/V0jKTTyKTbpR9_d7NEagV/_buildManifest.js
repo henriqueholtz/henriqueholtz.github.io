3d467d830363e663469b700b2f1adb6a20c00ab3
henriqueholtz <henrique_holtz@hotmail.com>
2022-03-28 07:34:58 +0000

    Deploying to gh-pages from @ henriqueholtz/henriqueholtz.github.io@e2401bc414b86195653abd8ae9f4b5d94c20ee7d 🚀

---
+++ _next/static/V0jKTTyKTbpR9_d7NEagV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3c3692e81f78c9c0.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
